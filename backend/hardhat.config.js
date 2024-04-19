e39d2a3f2323c7821f108a0d09550df678e8094e
Dipti Kumari <diptiku2002@gmail.com>
2024-04-20 00:17:52 +0530

    struc added

---
+++ backend/hardhat.config.js
@@ -0,0 +1,25 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-waffle");
+// import "@nomicfoundation/hardhat-web3-v4";
+require("@nomicfoundation/hardhat-web3-v4");
+const sepoliaPrivateKey = process.env.SEPOLIA_PRIVATE_KEY;
+module.exports = {
+  solidity: "0.8.24",
+
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/4f6f1969edfd4d219a76aa2d36019f32`,
+      accounts: [
+        "eed0a0759494fb2eea63e743bdbeae73236d507e0f89b0137935226355b7f3fd",
+      ],
+      network_id: 11155111,
+
+      gas: 2100000,
+      gasPrice: 8000000000,
+      saveDeployments: true,
+    },
+  },
+  paths: {
+    artifacts: "./artifacts",
+  },
+};
